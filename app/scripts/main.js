b5e98b025cc6cfae73932d04385209621dd66285
Gloryofthe80s <gloryoftheeighties@gmail.com>
2014-03-01 17:05:55 -0500

    commit before major overhaul

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     window.expensiveItems = new ExpensiveItemsCollection();
     window.cheapItems = new CheapItemsCollection();
-      
+
     var router = new MainRouter();
 
     Backbone.history.start();
--- app/scripts/models/cheap-items-collection.js
+++ app/scripts/models/cheap-items-collection.js
@@ -1,7 +1,7 @@
 var CheapItemsCollection = Backbone.Collection.extend({
     model: Item,
 
-    url: "https://api.etsy.com/v2/listings/active.js?limit=10&sort_on=price&tags=art,painting&min_price=40&max_price=100&includes=Images&api_key=gpby6hrhuzepnv0rx17946lk&callback=?",
+    url: "https://api.etsy.com/v2/listings/active.js?limit=10&tags=art,painting&min_price=40&max_price=100&includes=Images&api_key=gpby6hrhuzepnv0rx17946lk&callback=?",
 
     initialize: function() {
         this.fetch();
