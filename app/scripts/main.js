87690d37cce5378c11c43a817760bc4f18b7cc3c
Gloryofthe80s <gloryoftheeighties@gmail.com>
2014-03-02 17:40:30 -0500

    getting close to having randomized 'pairs' of items templated onto the page...

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     $('#start-quiz').button('loading');
     window.expensiveItems = new ExpensiveItemsCollection();
     window.cheapItems = new CheapItemsCollection();
+    window.actualQuiz = [];
 
     loadCollections = function() {
         cheapItems.fetch({
@@ -11,6 +12,16 @@ $(document).ready(function () {
             expensiveItems.fetch({
               success: function() {
                 console.log('collections loaded!')
+                expensiveItems = expensiveItems.shuffle();
+                cheapItems = cheapItems.shuffle();
+
+                for (var i = 0; i > 14; i++) {
+                    var staging = [expensiveItem[i], cheapItem[i]]
+                    staging.shuffle();
+
+                    //put the 'mini-array' pair in the actual array
+                    actualQuiz.push(staging);
+                };
                 $('#start-quiz').button('reset');
               }
           })
@@ -25,14 +36,11 @@ $(document).ready(function () {
             console.log('quiz loaded!');
 
             $('#quiz-wrapper').html('');
-
-            _.each(cheapItems.models, function(el, i) {
-                new ItemView({model: el});
-            });
-
-            _.each(expensiveItems.models, function(el, i) {
-                new ItemView({model: el});
-            });
+            _.each(actualQuiz, function(i) {
+                var aRow = new RowView();
+                aRow.items = actualQuiz[i];
+                aRow.render();
+            })
     });
 
 
